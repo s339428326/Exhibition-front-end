1bfbb26e4bcb1d2de3f6c6c7ba3177ec063851ba
yuling <gb2013429@gmail.com>
2023-08-05 20:28:25 +0800

    add index-artService+ swiper install

---
+++ src/components/Swiper.vue
@@ -0,0 +1,35 @@
+<template>
+    <swiper :navigation="true" :modules="modules" class="mySwiper">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+    </swiper>
+  </template>
+  <script>
+    // Import Swiper Vue.js components
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+  
+    // Import Swiper styles
+    import 'swiper/css';
+  
+    import 'swiper/css/navigation';
+  
+    // import './style.css';
+  
+    // import required modules
+    import { Navigation } from 'swiper/modules';
+  
+    export default {
+      components: {
+        Swiper,
+        SwiperSlide,
+      },
+      setup() {
+        return {
+          modules: [Navigation],
+        };
+      },
+    };
+  </script>
+  
