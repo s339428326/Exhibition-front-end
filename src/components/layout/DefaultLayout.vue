af501972101d0d1154241eb1325da5e60c31b18d
Stanley Wong <s339428326@gmail.com>
2023-08-14 17:30:13 +0800

    feature:add layout dynamic routing

---
+++ src/components/layout/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<script>
+    import AppHeader from './AppHeader.vue'
+    import AppFooter from './AppFooter.vue'
+    export default {
+        components: { AppHeader, AppFooter }
+    }
+</script>
+
+<template>
+    <AppHeader />
+    <router-view />
+    <AppFooter />
+</template>
+
+<style scoped></style>
