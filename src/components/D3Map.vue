b2f6c07fa3c42e3ffb06cad69983c696ad5df1ad
yuling <gb2013429@gmail.com>
2023-08-15 21:18:21 +0800

    map

--- src/components/D3Map.vue
+++ src/components/D3Map.vue
@@ -1,9 +1,7 @@
 <template>
     <h1>test</h1>
-    <svg
-        width="800"
-        height="500"
-    ></svg>
+    <!-- <div id="map-container"></div> -->
+    <svg id="map" class="border"></svg>
 </template>
 <style lang="scss">
     body {
@@ -23,4 +21,36 @@
 <script>
     import 'https://leafletjs.com/examples/choropleth/us-states.js'
     import '../assets/js/d3.js'
+    import * as d3 from "d3";
+import * as topojson from "topojson";
+export default {
+        mounted() {
+            this.draw()
+        },
+        methods: {
+            draw() {
+                let svg = d3
+                    .select('#map')
+                    .attr('width', 500)
+                    .attr('height', 600)
+
+                var projectmethod = d3.geoMercator().center([123.4, 24]).scale(8000)
+                var pathGenerator = d3.geoPath().projection(projectmethod)
+                d3.json('../COUNTY_MOI_1090820.json').then((data) => {
+                    const geometries = topojson.feature(data, data.objects['COUNTY_MOI_1090820'])
+
+                    svg.append('path')
+                    const paths = svg.selectAll('path').data(geometries.features)
+                    paths
+                        .enter()
+                        .append('path')
+                        .attr('d', pathGenerator)
+                        .attr('class', 'county')
+                        // 加上簡易版本 tooltip
+                        .append('title')
+                        .text((d) => d.properties['COUNTYNAME'])
+                })
+            }
+        }
+    }
 </script>
