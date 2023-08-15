967528051c4b52a0114f2e561e4f74e3a63c9b88
Stanley Wong <s339428326@gmail.com>
2023-08-15 18:21:28 +0800

    doc:add LikeButton and rebuild exhibition view  data state

--- src/views/ViewExhibition.vue
+++ src/views/ViewExhibition.vue
@@ -2,7 +2,7 @@
     <main class="view-exhibition">
         <section class="cover-banner mb-4">
             <img
-                :src="coverImage"
+                :src="data.coverImage"
                 alt="exh-banner-cover"
             />
         </section>
@@ -12,34 +12,32 @@
                 <!-- 展覽Title-->
                 <div class="border-bottom pb-2 mb-4 d-flex flex-wrap justify-content-between">
                     <div class="d-flex flex-column">
-                        <h1>{{ name }}</h1>
+                        <!-- 展覽名稱 -->
+                        <h1>{{ data.name }}</h1>
+                        <!-- 展覽時間 -->
                         <time
                             class="fs-4 fw-bold"
                             datetime="#"
-                            >{{ date }}
+                            >{{ data.date }}
                         </time>
                     </div>
                     <div>
-                        <button
-                            v-if="isHeartClick"
-                            class="btn btn-primary d-flex gap-2"
-                            @click="HeartHandler"
+                        <LikeButton
+                            :className="`btn d-flex p-2 gap-2 ${
+                                isHeartClick ? 'btn-primary' : 'btn-outline-primary'
+                            }`"
+                            :data="data"
+                            :isLike="isHeartClick"
+                            :handler="handleLike"
                         >
-                            <HeartIcon />
-                            收藏
-                        </button>
-                        <button
-                            v-else
-                            class="btn btn-outline-primary d-flex gap-2"
-                            @click="HeartHandler"
-                        >
-                            <HeartOutlineIcon />
-                            收藏
-                        </button>
+                            <template #content>
+                                <p>搜藏</p>
+                            </template>
+                        </LikeButton>
                     </div>
                 </div>
                 <p class="mb-4">
-                    {{ introduce }}
+                    {{ data.introduce }}
                 </p>
             </div>
             <div class="col col-md-4">
@@ -49,7 +47,7 @@
                         <ul>
                             <!-- fix: index => firebase id -->
                             <li
-                                v-for="(ticket, index) in tickGroup"
+                                v-for="(ticket, index) in data.tickGroup"
                                 :key="index"
                             >
                                 <div class="ticket-choose">
@@ -73,14 +71,11 @@
                             </li>
                         </ul>
                     </div>
-                    <!-- 綁定dataset  -->
-                    <!-- name:item, price:100 -->
                     <button
                         @click="addCartItemHandler"
                         class="btn btn-primary w-100 mb-2 addCartBtn"
                         type="button"
-                        :data-name="name"
-                        data-price="100"
+                        :data-name="data.name"
                         :disabled="cartBtnDisable"
                     >
                         {{ cartBtnName }}
@@ -90,48 +85,47 @@
                         to="/payment"
                         >前往結賬</RouterLink
                     >
+                    <!-- fix: change to item id -->
                 </div>
             </div>
         </section>
     </main>
 </template>
 <script>
+    //vue-router
     import { RouterLink } from 'vue-router'
+    //pinia
     import { useCartDataStore } from '../stores/cartData'
-    //1.取得axios 資料後渲染畫面
+    //components
+    import LikeButton from '../components/LikeButton.vue'
     export default {
         data() {
             return {
-                //pinia  狀態管理
+                //pinia  data manger
                 cartDataInstance: useCartDataStore(),
                 isHeartClick: false,
-                //view
-                name: '', //展覽名稱
-                coverImage: '', //展覽圖片
-                date: '2023/12/31 ~ 2024/01/01', //展覽日期
-                introduce: '', //展覽介紹
-                tickGroup: [
-                    { tickType: '單展票', price: 80 },
-                    { tickType: '套票', price: 150 }
-                ], //套票資訊
+                //get axios view data state
+                data: {},
+                //////////memo user interface choose tickType and price ////////
                 tickType: {}, //目前選擇套票
                 price: '0' //目前選擇套票價格
             }
         },
         computed: {
-            //加入購物車按鈕字樣控制
+            //control cart button text
             cartBtnName() {
                 if (this.cartBtnDisable) {
                     return '已加入購物車'
                 }
                 return '加入購物車'
             },
-            //加入購物車按鈕disable控制
+            //control add cart button disable
             cartBtnDisable() {
-                //需要新增日期不符合時 禁用
+                //feature: add item date disable button
                 const item = this.cartDataInstance.cartData.find(
                     (item) =>
-                        item.name === this.name && item.tickType.tickType === this.tickType.tickType
+                        item.name === this.data.name &&
+                        item.tickType.tickType === this.data.tickType.tickType
                 )
                 return item ? true : false
             }
@@ -139,44 +133,60 @@
         // axios get page data
         async created() {
             //fake get view data
-            this.name = '高雄駁二展覽｜人生紀念品'
-            this.introduce =
-                '人生中有許多珍貴的回憶，這些回憶可能透過一些際遇留存下來。在斷捨離的潮流趨勢下，我們開始思考自己真正需要保留的東西是什麼。對於許多人來說，重要的紀念品不一定是價值昂貴的物品，而是那些充滿情感和回憶的物品。可能是在某個重要的日子收到的花，讓你想起了某些時刻；可能是和寵物陪伴，在人生裡的一段時光；可能是在樹下一起望著天空的那天下午；可能是在獲得獎項被肯定的時刻，也可能是依舊想念的家鄉味道，這些物品不僅讓你回憶起過去的美好時光，也能夠讓你更感受到那些經歷真實性和重要。重要的是，留下這些紀念品並不是要填滿你的家，而是讓你在未來的某一天拿起它們時，能夠感受到那份特別的情感和回憶。你人生中最重要的紀念品是什麼？「人生紀念品」展覽邀請各領域設計師重新詮釋自身或受訪者的人生故事，透過設計手法將故事濃縮淬煉成一件件紀念品，引領觀者從設計作品中品味各種人生故事的酸甜滋味，展覽也邀請知名設計師及文字工作者分享影響他們人生的重要紀念品，期待民眾從第三者角度閱讀他人故事的同時，也回想起一些人生值得紀念的片段，讓記憶中的故事因為展覽有新一層的體會及滋味。'
-            this.coverImage = 'https://pier2.org/upload/others/files/NVT6218.jpg'
-            this.price = 100
-            this.tickGroup = [
-                { tickType: '單展票', price: 80 },
-                { tickType: '套票', price: 150 }
-            ]
-            //初始值給予 票種第一項
-            this.tickType = { tickType: '單展票', price: 80 }
-            //
+            this.data = {
+                name: '高雄駁二展覽｜人生紀念品',
+                date: '2023/12/31 ~ 2024/01/01',
+                type: '藝術',
+                coverImage: 'https://pier2.org/upload/others/files/NVT6218.jpg',
+                introduce:
+                    '人生中有許多珍貴的回憶，這些回憶可能透過一些際遇留存下來。在斷捨離的潮流趨勢下，我們開始思考自己真正需要保留的東西是什麼。對於許多人來說，重要的紀念品不一定是價值昂貴的物品，而是那些充滿情感和回憶的物品。可能是在某個重要的日子收到的花，讓你想起了某些時刻；可能是和寵物陪伴，在人生裡的一段時光；可能是在樹下一起望著天空的那天下午；可能是在獲得獎項被肯定的時刻，也可能是依舊想念的家鄉味道，這些物品不僅讓你回憶起過去的美好時光，也能夠讓你更感受到那些經歷真實性和重要。重要的是，留下這些紀念品並不是要填滿你的家，而是讓你在未來的某一天拿起它們時，能夠感受到那份特別的情感和回憶。你人生中最重要的紀念品是什麼？「人生紀念品」展覽邀請各領域設計師重新詮釋自身或受訪者的人生故事，透過設計手法將故事濃縮淬煉成一件件紀念品，引領觀者從設計作品中品味各種人生故事的酸甜滋味，展覽也邀請知名設計師及文字工作者分享影響他們人生的重要紀念品，期待民眾從第三者角度閱讀他人故事的同時，也回想起一些人生值得紀念的片段，讓記憶中的故事因為展覽有新一層的體會及滋味。',
+                tickGroup: [
+                    { tickType: '單展票', price: 80 },
+                    { tickType: '套票', price: 150 }
+                ],
+                address: '高雄市駁二藝術中心', //地址
+                position: {
+                    lat: 25.0338, //緯度
+                    lng: 121.5645 //經度
+                },
+                /////////非取得資訊
+                //初始值給予 票種第一項
+                tickType: { tickType: '單展票', price: 80 }
+            }
         },
         components: {
-            RouterLink
+            RouterLink,
+            LikeButton
         },
         methods: {
-            addCartItemHandler(e) {
-                console.log('test', this.price, this.tickGroup, this.name)
+            addCartItemHandler() {
+                //push to localStorage cart data item
                 const item = {
-                    name: e.target.dataset.name,
-                    date: this.date,
-                    coverImage: this.coverImage,
-                    tickType: this.tickType,
-                    price: this.tickType.price,
+                    name: this.data.name,
+                    date: this.data.date,
+                    coverImage: this.data.coverImage,
+                    tickType: this.data.tickType,
+                    price: this.data.tickType.price,
                     quantity: 1
                 }
-                console.log('[addCartItemHandler]', item)
+                //push to cart store cartData
                 this.cartDataInstance.addCartItem(item)
             },
             changeTickDate(e) {
+                //get dataset-tickgroupindx to tickGroup index
                 const tickGroupIndex = e.target.dataset.tickgroupindex
-                this.tickType = this.tickGroup[tickGroupIndex]
-                // this.cartBtnDisable = true
-                // console.log(this.cartBtnDisable)
+                //change ticket type state
+                this.data.tickType = this.data.tickGroup[tickGroupIndex]
             },
-            HeartHandler() {
+            handleLike() {
+                //demo for HeartIcon to HeartOutlineIcon
                 this.isHeartClick = !this.isHeartClick
+                console.log(
+                    '[Like btn]',
+                    this.isHeartClick,
+                    this.data,
+                    'feature: 等待開發Like FireBase 資料 及 新增 pinia store使用'
+                )
             }
         }
     }
