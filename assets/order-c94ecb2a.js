f242ccc423137bcce4291f2cd5bfcf87f506276d
Stanley Wong <s339428326@gmail.com>
2023-09-17 23:05:26 +0800

    Updates

--- assets/order-c94ecb2a.js
+++
@@ -1,1 +0,0 @@
-import{D as o}from"./index-d614a8a0.js";const t=async e=>{try{return o.post("https://evening-hollows-08215-2bb8e1b9252d.herokuapp.com/api/v1/order/",e).data}catch(r){console.log(r)}},c=async e=>{try{return o.get(`https://evening-hollows-08215-2bb8e1b9252d.herokuapp.com/api/v1/order/user/${e}`)}catch(r){console.log(r)}};export{t as c,c as g};
