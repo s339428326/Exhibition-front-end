dc356550d20d27d243920b8c3ba0a6049ea92f2e
翁柏豐 <s339428326@gmail.com>
2023-10-15 19:21:51 +0800

    doc:upadte readme.md

--- src/views/TestQR.vue
+++ src/views/TestQR.vue
@@ -1,18 +1,65 @@
 <script setup>
-    import { ref, onMounted } from 'vue'
-    //////////////
+    //注意使用ver.5 因為開發團隊文件未更新完全, 請直接參考以下API文件
+    // https://vue-qrcode-reader.netlify.app/api/QrcodeStream.html
+
+    import { ref } from 'vue'
+    //Not Vue-qrcode-reader team recommend method close camera
+    const camera = ref(false)
 
     const scannedData = ref('')
-    const onDecode = (result) => {
-        alert(result)
-        scannedData.value = result
+
+    //記入該名檢票員掃描票卷(工作人員工作記錄非主要功能)
+    const ticketCheckerData = ref([])
+
+    //
+
+    const onDetect = (detected) => {
+        scannedData.value = detected[0]?.rawValue
+        if (scannedData) alert(JSON.stringify(detected, null, 2))
+        // if (scannedData) camera.value = false
+    }
+
+    const closeCamera = () => {
+        camera.value = !camera.value
+    }
+
+    //refer(QR code 檢測框)
+    //https://vue-qrcode-reader.netlify.app/demos/CustomTracking.html
+    const paintOutline = (detectedCodes, ctx) => {
+        for (const detectedCode of detectedCodes) {
+            const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
+
+            ctx.strokeStyle = 'red'
+
+            ctx.beginPath()
+            ctx.moveTo(firstPoint.x, firstPoint.y)
+            for (const { x, y } of otherPoints) {
+                ctx.lineTo(x, y)
+            }
+            ctx.lineTo(firstPoint.x, firstPoint.y)
+            ctx.closePath()
+            ctx.stroke()
+        }
     }
 </script>
 
 <template>
     <div>
         <h1>Test</h1>
-        <qrcode-stream @decode="onDecode"></qrcode-stream>
+        {{ camera }}
+        <button
+            type="button"
+            @click="closeCamera"
+        >
+            Close Testing
+        </button>
+
         <div>Scanned Data: {{ scannedData }}</div>
+        <qrcode-stream
+            v-if="camera"
+            :track="paintOutline"
+            @camera-on="onReady"
+            @detect="onDetect"
+        ></qrcode-stream>
     </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 
 import vue from '@vitejs/plugin-vue'
 
@@ -8,7 +9,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     // base: '/Repository 的名稱/'
     base: '/Exhibition-front-end/',
-    plugins: [vue()],
+    plugins: [vue(), mkcert()], //,
     resolve: {
         alias: {
             '~': fileURLToPath(new URL('./src', import.meta.url))
@@ -17,8 +18,9 @@ export default defineConfig({
     server: {
         // 啟動 server 時預設開啟的頁面
         // open: '/',
-        // host: '192.168.0.12',
-        // port: 5173
+        https: true,
+        // host: '192.168.0.12', //RWD 同網域下可以連線設定
+        port: 5173
     },
     build: {
         outDir: 'dist',
