534ce718302e7f19c31c2310325911c1e7d1b0f1
翁柏豐 <s339428326@gmail.com>
2023-10-20 01:49:10 +0800

    doc:take out HerokuStart.vue and Change naming to wake

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <script setup>
-    import HerokuStart from './components/Heroku/HerokuStart.vue'
+    import { onMounted } from 'vue'
+    import { wakeAPIServer } from './api/wake'
+    import { useAlert } from './stores/alertSlice'
+
+    const { callAlert } = useAlert()
+
+    const callServer = async () => {
+        callAlert({ title: '請稍等！, 喚醒後端服務！', type: 'alert' })
+        try {
+            const res = await wakeAPIServer()
+            callAlert({ title: '成功連結後端', type: 'check' })
+            return res
+        } catch (error) {
+            console.error(error)
+            callAlert({ title: '注意! Heroku 連結失敗', type: 'errors' })
+            return error
+        }
+    }
+
+    onMounted(() => {
+        callServer()
+    })
 </script>
 
 <template>
     <router-view />
-    <HerokuStart />
 </template>
 
 <style scoped></style>
